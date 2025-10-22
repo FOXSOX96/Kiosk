2008bda28c6d78541802f143932099cc5c987e46
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-22 20:19:47 +0900

    Refactor : menuNo할당 방식 변경
    키 값에 맞게 메뉴명 안내.
    범위는 배열에 사이즈에 맞게 설정.

--- src/essential/level2/Main.java
+++ src/essential/level2/Main.java
@@ -15,9 +15,10 @@ public class Main {
         Map<Double, MenuItem> getMenuAll = menuID.getMenuAll();
 
 
+
         /*속성*/
-        int selectNo = 99999; /*번호선택*/
-        int menuNo = 99999; /*메뉴번호*/
+        double selectNo = 99999; /*번호선택*/
+        double menuNo = 99999; /*메뉴번호*/
 
         /* 메뉴선택: 0이 입력되면 종료되는 반복문시작*/
         while (selectNo != 0) {
@@ -29,47 +30,32 @@ public class Main {
                 System.out.printf("%-4s | %-14s (%4.1f) - %s\n",
                         key, item.getMenuName(), item.getMenuPrice(), item.getMenuDetail());
             }
-            System.out.println("0.   | 종료");
+            System.out.println("0. 종료");
 
             /*입력-selectNo할당*/
-            System.out.println("\n메뉴를 선택해주세요");
+            System.out.println("메뉴의 번호를 선택해주세요");
             selectNo = getSelectNo(sc);
 
             /*메뉴번호선택-menuNo할당*/
-            switch (selectNo) {
-                case 1:
-                    menuNo = 1; //"ShackBurger"
-                    System.out.println("ShackBurger 를 선택하였습니다.\n");
-                    break;
-                case 2:
-                    menuNo = 2; //"SmokeShack"
-                    System.out.println("SmokeShack 를 선택하였습니다.\n");
-                    break;
-                case 3:
-                    menuNo = 3; // "Cheeseburger"
-                    System.out.println("Cheeseburger 를 선택하였습니다.\n");
-                    break;
-                case 4:
-                    menuNo = 4; // "Hamburger"
-                    System.out.println("Hamburger 를 선택하였습니다.\n");
-                    break;
-                case 0:
-                    System.out.println("프로그램을 종료합니다.\n");
-                    break;
-                default:
-                    System.out.println("선택된 메뉴가 없습니다.\n");
-                    break;
+            if (selectNo == 0) {
+                System.out.println("프로그램을 종료합니다.\n");
+            } else if(selectNo <= 1+0.1*getMenuAll.size()) {
+                menuNo = selectNo;
+                System.out.println(menuID.getMenu(selectNo).getMenuName() + "을 선택하였습니다.");
+            } else {
+                System.out.println("메뉴와 일치하는 숫자를 입력해야 합니다.");
             }
+
 
         }
 
     }
 
     /*selectNo-스캐너입력 할당*/
-    public static int getSelectNo(Scanner sc) {
-        int selectNo;
+    public static double getSelectNo(Scanner sc) {
+        double selectNo;
         try {
-            selectNo = sc.nextInt();
+            selectNo = sc.nextDouble();
         } catch (InputMismatchException e) {
             throw new IllegalArgumentException("메뉴와 일치하는 숫자를 입력해야 합니다.");
         }
