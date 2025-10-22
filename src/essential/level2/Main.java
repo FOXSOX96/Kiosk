ffe9ec151dae34ea550948da510fdc329252d6dc
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-22 18:55:42 +0900

    Refactor : MenuID 키값 타입 double로 변경 => 1.1, 1.2, 2.1 분류체계사용
    Feat : menuMap 키오스크에 자동표기

--- src/essential/level2/Main.java
+++ src/essential/level2/Main.java
@@ -1,13 +1,19 @@
 package essential.level2;
 
-import java.util.*;
+
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        /*Lv 1. 기본적인 키오스크를 프로그래밍해보자*/
+        /*MenuID클래스 객체*/
+        MenuID menuID = new MenuID();
+        Map<Double, MenuItem> getMenuAll = menuID.getMenuAll();
+
 
         /*속성*/
         int selectNo = 99999; /*번호선택*/
@@ -17,14 +23,16 @@ public class Main {
         while (selectNo != 0) {
 
             System.out.println("[SHAKESHACK MENU ]");
-            System.out.println("1. ShackBurger  | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. SmokeShack   | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. Cheeseburger | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. Hamburger    | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-            System.out.println("0. 종료          | 종료");
+            for (Map.Entry<Double, MenuItem> entry : getMenuAll.entrySet()) {
+                Double key = entry.getKey();
+                MenuItem item = entry.getValue();
+                System.out.printf("%-4s | %-14s (%4.1f) - %s\n",
+                        key, item.getMenuName(), item.getMenuPrice(), item.getMenuDetail());
+            }
+            System.out.println("0.   | 종료");
 
             /*입력-selectNo할당*/
-            System.out.println("메뉴를 선택해주세요");
+            System.out.println("\n메뉴를 선택해주세요");
             selectNo = getSelectNo(sc);
 
             /*메뉴번호선택-menuNo할당*/
@@ -63,7 +71,6 @@ public class Main {
         try {
             selectNo = sc.nextInt();
         } catch (InputMismatchException e) {
-            sc.nextLine();
             throw new IllegalArgumentException("메뉴와 일치하는 숫자를 입력해야 합니다.");
         }
         return selectNo;
