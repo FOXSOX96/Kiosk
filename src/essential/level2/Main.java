10b0e80f6b7dd5ba9847d988fcc9a222fe805b57
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-22 21:52:31 +0900

    refactor : Level2의 클래스 기준에 맞게 2개로 재설정

--- src/essential/level2/Main.java
+++ src/essential/level2/Main.java
@@ -1,9 +1,7 @@
 package essential.level2;
 
 
-import java.util.InputMismatchException;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -39,10 +37,14 @@ public class Main {
             /*메뉴번호선택-menuNo할당*/
             if (selectNo == 0) {
                 System.out.println("프로그램을 종료합니다.\n");
-            } else if(selectNo <= 1+0.1*getMenuAll.size()) {
+            } else if (selectNo <= 1 + 0.1 * getMenuAll.size()) {
                 menuNo = selectNo;
-                System.out.println(menuID.getMenu(selectNo).getMenuName() + "을 선택하였습니다.");
-            } else {
+                menuID.getMenu(selectNo)
+                        .ifPresentOrElse(
+                                item -> System.out.println(item.getMenuName() + "을 선택하였습니다."),
+                                ()   -> System.out.println("해당 메뉴가 존재하지 않습니다.")
+                        );
+            }else {
                 System.out.println("메뉴와 일치하는 숫자를 입력해야 합니다.");
             }
 
@@ -50,6 +52,38 @@ public class Main {
         }
 
     }
+
+    public static class MenuID {
+
+        /*메뉴 아이디 할당 : MenuID =double*/
+        private Map<Double, MenuItem> menuMap = new LinkedHashMap<>();
+
+        /*생성자*/
+        public MenuID() {
+            /*메뉴 추가*/
+            menuMap.put(1.1, new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+            menuMap.put(1.2, new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+            menuMap.put(1.3, new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+            menuMap.put(1.4, new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        }
+
+        /*게터*/
+        public Optional<MenuItem> getMenu(Double MenuID) {
+            return Optional.ofNullable(menuMap.get(MenuID));
+        }
+
+        /*게터 전체*/
+        public Map<Double, MenuItem> getMenuAll() {
+            return menuMap;
+        }
+
+        /*세터*/
+        public void setMenuMap(Map<Double, MenuItem> menuMap) {
+            this.menuMap = menuMap;
+        }
+    }
+
 
     /*selectNo-스캐너입력 할당*/
     public static double getSelectNo(Scanner sc) {
