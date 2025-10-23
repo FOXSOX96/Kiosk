7b1b599d21a80975e64d72bf6d4e61eb70400f09
WonTaeJun <dnjsxonws94@gmail.com>
2025-10-23 15:45:37 +0900

    Refactor : MenuID클래스 Kiosk클래스로 변경, Main클래스의 반복문 Kiosk로 이동

--- src/essential/level3/Kiosk.java
+++ src/essential/level3/Kiosk.java
@@ -1,8 +1,10 @@
 package essential.level3;
 
+import java.util.InputMismatchException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class Kiosk {
 
@@ -20,6 +22,59 @@ public class Kiosk {
 
     }
 
+    /*메뉴선택 반복문*/
+    public void start (Map<Double, MenuItem> getMenuAll, Scanner sc, Kiosk kiosk) {
+
+        /*속성*/
+        double selectNo = 99999; /*번호선택*/
+        double menuNo = 99999; /*메뉴번호*/
+
+        /* 메뉴선택: 0이 입력되면 종료되는 반복문시작*/
+        while (selectNo != 0) {
+
+            System.out.println("[SHAKESHACK MENU ]");
+            for (Map.Entry<Double, MenuItem> entry : getMenuAll.entrySet()) {
+                Double key = entry.getKey();
+                MenuItem item = entry.getValue();
+                System.out.printf("%-4s | %-14s (%4.1f) - %s\n",
+                        key, item.getMenuName(), item.getMenuPrice(), item.getMenuDetail());
+            }
+            System.out.println("0. 종료");
+
+            /*입력-selectNo할당*/
+            System.out.println("메뉴의 번호를 선택해주세요");
+            selectNo = getSelectNo(sc);
+
+            /*메뉴번호선택-menuNo할당*/
+            if (selectNo == 0) {
+                System.out.println("프로그램을 종료합니다.\n");
+            } else if (selectNo <= 1 + 0.1 * getMenuAll.size()) {
+                menuNo = selectNo;
+                kiosk.getMenu(selectNo).ifPresentOrElse(
+                        item -> System.out.println(item.getMenuName() + "을 선택하였습니다."),
+                        () -> System.out.println("메뉴와 일치하는 숫자를 입력해야 합니다.")
+                );
+            } else {
+                System.out.println("메뉴와 일치하는 숫자를 입력해야 합니다.");
+            }
+
+
+        }
+
+
+    }
+
+    /*selectNo-스캐너입력 할당*/
+    public static double getSelectNo(Scanner sc) {
+        double selectNo;
+        try {
+            selectNo = sc.nextDouble();
+        } catch (InputMismatchException e) {
+            throw new IllegalArgumentException("메뉴와 일치하는 숫자를 입력해야 합니다.");
+        }
+        return selectNo;
+    }
+
     /*게터*/
     public Optional<MenuItem> getMenu(Double MenuID) {
         return Optional.ofNullable(menuMap.get(MenuID));
@@ -35,4 +90,5 @@ public class Kiosk {
     public void setMenuMap(Map<Double, MenuItem> menuMap) {
         this.menuMap = menuMap;
     }
+
 }
